66fde8be1b6ea0a75c4e27e561fb2b7bd5fb292d
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-07-26 22:36:25 -0300

    adjustments and enhancement script instalation

--- shellscripts/installPrograms.sh
+++ shellscripts/installPrograms.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
 
+# Função para instalar yay caso não esteja instalado
+install_yay() {
+  if ! command -v yay &> /dev/null; then
+    echo "yay não encontrado. Instalando yay..."
+    echo
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd -
+    rm -rf /tmp/yay
+  else
+    echo "yay já está instalado."
+    echo
+  fi
+}
+
+# Instalar yay se necessário
+install_yay
+
 # Atualizar pacotes existentes
 echo "Atualizando pacotes existentes..."
-yay
+yay -Syu --noconfirm
 
 # Instalar pacotes necessários, listados linha por linha
-echo "Instalando todos os pacotes necessários..."
-yay -S \
+echo "Instalando os pacotes..."
+yay -S --noconfirm \
+  hyprland \
+  hyprpaper \
+  hyprlock \
+  xdg-desktop-portal-hyprland \
+  hyprpolkitagent \
+  hyprsysteminfo \
+  hyprland-qt-support \
+  hyprcursor \
+  hyprutils \
+  hyprlang \
+  aquamarine \
+  hyprgraphics \
+  hyprpicker \
+  hyprshot \
   kitty \
   nautilus \
   firefox \
   thunderbird \
-  hyprpicker \
-  rofi \
+  rofi-wayland \
   smile \
-  hyprshot \
-  hyprlock \
   wleave \
-  code \
+  visual-studio-code-bin \
   telegram-desktop \
-  wasistlos \
+  zapzap \
   waybar \
   waypaper \
   kvantum \
   kvantum-qt5 \
   kvantum-qt6 \
-  nwg-look
+  nwg-look \
+  anyrun-git \
+  fastfetch \
+  swaync 
 
 echo "Todos os programas foram instalados com sucesso!"
