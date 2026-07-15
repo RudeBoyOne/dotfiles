85cdc64fef5e53032a3f609e8112c21876a33260
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2026-07-14 22:57:48 -0300

    :recycle: refactor: adjustment name fuction

--- .config/hypr/components/rules/window-rule.lua
+++ .config/hypr/components/rules/window-rule.lua
@@ -2,59 +2,59 @@
 -- FLOAT, SIZE AND CENTER WINDOWS
 -- =========================================================================
 -- Calculadora
-hl.windowrule({ class = "org.gnome.Calculator" }, {
+hl.window_rule({ class = "org.gnome.Calculator" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 360, 616 } },
 	{ prop = "center" },
 })
 
 -- Nautilus
-hl.windowrule({ class = "org.gnome.Nautilus" }, {
+hl.window_rule({ class = "org.gnome.Nautilus" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 996, 655 } },
 	{ prop = "center" },
 })
 
 -- Kitty
-hl.windowrule({ class = "kitty" }, {
+hl.window_rule({ class = "kitty" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 875, 541 } },
 	{ prop = "center" },
 })
 
 -- File Roller
-hl.windowrule({ class = "org.gnome.FileRoller" }, {
+hl.window_rule({ class = "org.gnome.FileRoller" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 819, 562 } },
 	{ prop = "center" },
 })
 
 -- Solanum (Janela principal)
-hl.windowrule({ class = "org.gnome.Solanum" }, {
+hl.window_rule({ class = "org.gnome.Solanum" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 360, 294 } },
 	{ prop = "move", value = { 1552, 42 } },
 })
 
 -- Solanum (Preferências)
-hl.windowrule({ class = "org.gnome.Solanum", title = "Preferências" }, {
+hl.window_rule({ class = "org.gnome.Solanum", title = "Preferências" }, {
 	{ prop = "center" },
 })
 
 -- LocalSend
-hl.windowrule({ class = "localsend" }, {
+hl.window_rule({ class = "localsend" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 658, 614 } },
 	{ prop = "center" },
 })
 
 -- Remote Viewer
-hl.windowrule({ class = "remote-viewer" }, {
+hl.window_rule({ class = "remote-viewer" }, {
 	{ prop = "float" },
 })
 
 -- Xdg-desktop-portal-gtk (Regra genérica da primeira seção)
-hl.windowrule({ class = "Xdg-desktop-portal-gtk" }, {
+hl.window_rule({ class = "Xdg-desktop-portal-gtk" }, {
 	{ prop = "float" },
 })
 
@@ -62,12 +62,12 @@ hl.windowrule({ class = "Xdg-desktop-portal-gtk" }, {
 -- FOCUS PROBLEMS (IntelliJ / JetBrains & XWayland)
 -- =========================================================================
 
-hl.windowrule({ xwayland = true }, {
+hl.window_rule({ xwayland = true }, {
 	{ prop = "noinitialfocus" },
 	{ prop = "noblur" },
 })
 
-hl.windowrule({ class = "^(.*jetbrains.*)$", title = "^\\s$" }, {
+hl.window_rule({ class = "^(.*jetbrains.*)$", title = "^\\s$" }, {
 	{ prop = "nofocus" },
 })
 
@@ -76,14 +76,14 @@ hl.windowrule({ class = "^(.*jetbrains.*)$", title = "^\\s$" }, {
 -- =========================================================================
 
 -- Para "Xdg-desktop-portal-gtk" (com X maiúsculo)
-hl.windowrule({ class = "Xdg-desktop-portal-gtk" }, {
+hl.window_rule({ class = "Xdg-desktop-portal-gtk" }, {
 	{ prop = "noshadow" },
 	{ prop = "decorate", value = false },
 	{ prop = "dimaround" },
 })
 
 -- Para "xdg-desktop-portal-gtk" (com x minúsculo)
-hl.windowrule({ class = "xdg-desktop-portal-gtk" }, {
+hl.window_rule({ class = "xdg-desktop-portal-gtk" }, {
 	{ prop = "float" },
 	{ prop = "size", value = { 932, 670 } },
 	{ prop = "center" },
