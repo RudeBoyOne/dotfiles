43fe5eaecf9b64fb759af3a5d10ad7b543e97145
Lucas Ferreira Nogueira <dev@lucasfncode.com.br>
2026-08-22 10:23:42 -0300

    :recycle: refactor: startup nordvpn icon tray

--- .config/hypr/components/startup/launch.lua
+++ .config/hypr/components/startup/launch.lua
@@ -8,6 +8,7 @@ local configDbus = "dbus-update-activation-environment --systemd WAYLAND_DISPLAY
 local confiQt = "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME"
 local udiskie = "uswm-app -s s -- udiskie"
 local vicinae = "vicinae server"
+local nordvpnTray = "nordvpn status"
 
 hl.on("hyprland.start", function ()
   hl.exec_cmd(udiskie)
@@ -19,4 +20,5 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd(configDbus)
   hl.exec_cmd(confiQt)
   hl.exec_cmd(vicinae)
+  hl.exec_cmd(nordvpnTray)
 end)
